f2c5ff4c099278e421b2bcbe6d6ccb6b945d448a
fl0rijan <florijan2001@gmail.com>
2025-04-20 22:13:20 +0200

    Displaying route(s) and his arrival times

--- src/busTrips.java
+++ src/busTrips.java
@@ -8,18 +8,19 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Objects;
 
+import static java.lang.Math.abs;
+
 public class busTrips {
     public static void main(String[] args) throws IOException {
         if (args.length == 3) {
+            ArrayList<String> routes = new ArrayList<>();
+            ArrayList<ArrayList<String>> route_times = new ArrayList<>();
+
             LocalTime Current_time = LocalTime.now();
 
             String id_postaje = args[0];
             String st_nasljednjih_postaj = args[1];
             String oblika_casa = args[2];
-
-            System.out.println(id_postaje);
-            System.out.println(st_nasljednjih_postaj);
-            System.out.println(oblika_casa);
 
             GtfsReader reader = new GtfsReader();
             reader.setInputLocation(new File("gtfs"));
@@ -30,37 +31,60 @@ public class busTrips {
             reader.run();
 
             Agency[] agency = store.getAllAgencies().toArray(new Agency[0]);
-            AgencyAndId id_stop = AgencyAndId.convertFromString(agency[0].getId()+"_"+id_postaje);
+            AgencyAndId id_stop = AgencyAndId.convertFromString(agency[0].getId() + "_" + id_postaje);
 
             Stop stop = store.getStopForId(id_stop);
-            String stop_name = stop.getName();
-            System.out.println("Postajališče "+ stop_name);
+            if (stop == null) {
+                System.out.println("Stop is null");
+                System.exit(-1);
+            }
 
+            String stop_name = stop.getName();
+            System.out.println("Postajališče " + stop_name);
             StopTime[] stop_times = store.getAllStopTimes().toArray(new StopTime[0]);
 
-            for (StopTime times : stop_times){
+            for (StopTime times : stop_times) {
                 StopLocation stop_id = times.getStop();
-                if (stop_id == stop){
+                if (stop_id == stop) {
                     Trip trip_id = times.getTrip();
                     Route route = trip_id.getRoute();
 
                     LocalTime arrival_time = LocalTime.ofSecondOfDay(times.getArrivalTime());
+                    String hour = String.valueOf(arrival_time.getHour());
+                    String minute = String.valueOf(arrival_time.getMinute());
                     int calculation = times.getArrivalTime() - Current_time.toSecondOfDay();
-                    int relative_time = (Current_time.toSecondOfDay() - times.getArrivalTime()) / 60;
+                    int relative_time = abs((Current_time.toSecondOfDay() - times.getArrivalTime()) / 60);
+                    String time = "";
 
-                    if(calculation <= 7200 && calculation >= 0){
-                        System.out.println(route.getShortName());
-                        if(Objects.equals(oblika_casa, "absolute")){
-                            System.out.println(arrival_time);
-                        }
-                        else if(Objects.equals(oblika_casa, "relative")){
-                            System.out.println(relative_time + "min");
+                    if (calculation <= 7200 && calculation >= 0) {
+                        String route_name = route.getShortName();
+                        //System.out.println(Integer.parseInt(route_name));
+
+                        if (Objects.equals(oblika_casa, "absolute")) {
+                            time = hour + ":" + minute;
+                        } else if (Objects.equals(oblika_casa, "relative")) {
+                            time = relative_time + "min";
                         }
 
+                        int index = routes.indexOf(route_name);
+
+                        if (index == -1) {
+                            routes.add(route_name);
+                            ArrayList<String> _times = new ArrayList<>();
+                            _times.add(time);
+                            route_times.add(_times);
+                        } else {
+                            if (route_times.get(index).size() < Integer.parseInt(st_nasljednjih_postaj)) {
+                                route_times.get(index).add(time);
+                            }
+                        }
                     }
                 }
             }
 
+            for (int i = 0; i < routes.size(); i++) {
+                System.out.println(routes.get(i) + ": " + String.join(",", route_times.get(i)));
+            }
         } else {
             System.err.println("id_postaje st.nasljednjih_postaj relative/absolute");
             System.exit(-1);
